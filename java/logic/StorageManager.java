88a427c20595d22e9897169e19cfbd109ebc9015
Your <am4063@g.rit.edu>
2024-02-08 09:12:09 -0500

    Storage Manager stubbed out

--- java/logic/StorageManager.java
+++ java/logic/StorageManager.java
@@ -1,3 +1,22 @@
 public class StorageManager {
+    public void getRecord_primarykey(){
+
+    }
+    public void getPage_table(){
+        
+    }
+    public void getRecords_tablenumber(){
+        
+    }
+    public void insertRecord_table (){
+        
+    }
+    
+    public void deleteRecord_primarykey(){
+
+    }
+    public void updateRecord_primarykey(){
+        
+    }
     
 }
