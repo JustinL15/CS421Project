16a25640af09da36c93e6ea37f9f80033fd548b9
Justin Lin <jl4682@rit.edu>
2024-02-10 23:48:11 -0500

    basic implementaion of insert

--- java/logic/Parser.java
+++ java/logic/Parser.java
@@ -14,13 +14,23 @@ public class Parser {
         
     }
     public void insert_values(Table table, String[] order, String[] values){
-        Map<String, Integer> dictionary = new HashMap<>();
-        for (Attribute attr: table.getAttributes()) {
+        Map<String, Integer> columnOrder = new HashMap<>();
+        Attribute[] tableCol = table.getAttributes();
+        String[] correctedVals = new String[values.length];
 
+        for (int i = 0; i < tableCol.length; i++) {
+            columnOrder.put(tableCol[i].getName(), i);
         }
-        for (String spec: order) {
-
+        for (int i = 0; i < order.length; i++) {
+            Integer orderNum = columnOrder.get(order[i]);
+            if (orderNum == null) {
+                // return an error
+                return;
+            }
+            correctedVals[orderNum] = values[i];
         }
+        // convert data into this function
+        sM.insertRecord_table();
     }
     public void print_display_schema(){
 
--- java/structures/Attribute.java
+++ java/structures/Attribute.java
@@ -9,4 +9,7 @@ public class Attribute {
     public Byte[] toBinary(){
         return null;
     };
+    public String getName(){
+        return null;
+    };
 }
