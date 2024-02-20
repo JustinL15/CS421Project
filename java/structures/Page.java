a0bbe525002700e7b261a8a8267a3e4cdbe7ec57
Justin Lin <jl4682@rit.edu>
2024-02-20 17:58:18 -0500

    made record more simple to use and made a simple page

--- java/structures/Page.java
+++ java/structures/Page.java
@@ -3,19 +3,42 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Page {
-    private ByteBuffer data;
     private List<Record> records;
-    private int frontPtr;
-    private int freePtr;
-    private List<Integer[]> freeArray = new ArrayList<>();
     private Table template;
 
-    public Page(int max_size, Table template) {
+    public Page(Table template, List<Record> records) {
+        this.records = records;
         this.template = template;
-        this.records = new ArrayList<>();
-        this.freePtr = max_size - 1;
-        this.frontPtr = 4;
-        this.data = ByteBuffer.allocate(max_size);
-        data.putInt(0);
     }
+
+    public Page(Table template, byte[] data) {
+        this.records = new ArrayList<Record>();
+        this.template = template;
+
+        ByteBuffer buffer = ByteBuffer.wrap(data);
+        int numRecords = buffer.getInt();
+
+        for (int i = 0; i < numRecords; i++) {
+            records.add(new Record(template, buffer));
+        }
+    }
+
+    public byte[] toByte(int max_size) {
+        ByteBuffer buffer =  ByteBuffer.allocate(max_size);
+        buffer.putInt(records.size());
+        for (Record record: records) {
+            buffer.put(record.toByte());
+        }
+        return buffer.array();
+    }
+
+    public List<Record> getRecords() {
+        return records;
+    }
+
+    public Table getTemplate() {
+        return template;
+    }
+
+
 }
