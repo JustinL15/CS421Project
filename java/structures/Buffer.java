77016907bbab1a006fce2fd7cc291b3df09e6040
Clarke Kennedy <clarkek0422@gmail.com>
2024-02-21 18:01:18 -0500

    wrote read method for Buffer

--- java/structures/Buffer.java
+++ java/structures/Buffer.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -7,16 +10,54 @@ public class Buffer {
     private Catalog catalog;
     private String databaseLocation;
 
-    public Buffer(Catalog catalog, String databaseLocation){
+    public Buffer(Catalog catalog, String databaseLocation) {
         this.pages = new LinkedList<Page>();
         this.catalog = catalog;
         this.databaseLocation = databaseLocation;
     }
-    public Page read(String tableName, int pageNumber){
-        int tableNumber = this.catalog.getTableByName(tableName).getNumber();
-        String tableLocation = this.databaseLocation;
-        return null;
+
+    public Page read(String tableName, int pageNumber) {
+        Table table = catalog.getTableByName(tableName);
+        for (Page page : pages) {
+            if (page.getPageNumber() == pageNumber) {
+                pages.remove(page);
+                pages.add(page);
+                return page;
+            }
+        }
+
+        int tableNumber = table.getNumber();
+        String tableLocation = databaseLocation + File.pathSeparator + tableNumber;
+        File tableFile = new File(tableLocation);
+        RandomAccessFile tableAccessFile;
+        try {
+            tableAccessFile = new RandomAccessFile(tableFile, "r");
+        } catch (FileNotFoundException e) {
+            System.out.println("No file at " + tableLocation);
+            return null;
+        }
+
+        int pageSize = catalog.getPageSize();
+        byte[] bytes = new byte[pageSize];
+        try {
+            tableAccessFile.read(bytes, pageNumber * pageSize, pageSize);
+            tableAccessFile.close();
+        } catch (IOException e) {
+            System.out.println("IOException when reading from table " + tableNumber);
+            return null;
+        }
+        Page page = new Page(table, bytes, pageNumber);
+        pages.add(page);
+        if (pages.size() > catalog.getBufferSize()) {
+            Page lruPage = pages.remove();
+            write(lruPage);
+        }
+        return page;
     }
-    private void write(){}
-    private void purge(){}
+
+    private void write(Page page) {
+    }
+
+    private void purge() {
+    }
 }
