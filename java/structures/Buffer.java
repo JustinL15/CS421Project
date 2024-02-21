f1625de429f270185be5758cfe1f40f0d770807e
Clarke Kennedy <clarkek0422@gmail.com>
2024-02-21 16:53:48 -0500

    added read stub to Buffer

--- java/structures/Buffer.java
+++ java/structures/Buffer.java
@@ -1,3 +1,14 @@
+import java.util.Queue;
+
 public class Buffer {
-    
+    private Queue<Page> pages;
+
+    public Buffer(){
+        
+    }
+    public Page read(String tableName, int pageNumber){
+        return null;
+    }
+    private void write(){}
+    private void purge(){}
 }
