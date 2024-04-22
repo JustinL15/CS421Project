2190f52e19915532798f2a553971913621b2031f
Your <am4063@g.rit.edu>
2024-04-22 02:16:43 -0400

    Edits of BPlusTreeNode

--- java/structures/BPlusTreeNode.java
+++ java/structures/BPlusTreeNode.java
@@ -61,18 +61,17 @@ public class BPlusTreeNode implements HardwarePage {
         this.children = children;
     }
 
-    
-    public BPlusTreeNode insert(Object key, int[] pointer, Buffer buffer ) {
+    public BPlusTreeNode insert(int key, int[] pointer, Buffer buffer) {
         if (isLeaf) {
-            int index = binarySearch(this.keys, key);
-            this.keys.add(index, key);
-            this.pointers.add(index, key);
+            int index = binarySearch(keys, key);
+            keys.add(index, key);
+            pointers.add(index, pointer);
             if (keys.size() > maxSize) {
-                List<List<object>> splitKeys = splitArrayList(keys);
-                List<List<int[]>> splitPointers = splitArrayList(pointers)
+                List<List<Integer>> splitKeys = splitArrayList(keys);
+                List<List<int[]>> splitPointers = splitArrayList(pointers);
                 this.keys = splitKeys.get(0);
                 this.pointers = splitPointers.get(0);
-                BPlusTreeNode newNode = new BPlusTreeNode(this.isLeaf, this.maxSize);
+                BPlusTreeNode newNode = new BPlusTreeNode(this.isLeaf, this.maxSize, this.template);
                 newNode.setKeys(splitKeys.get(1));
                 newNode.setPointers(splitPointers.get(1));
                 newNode.setParent(this.parent);
@@ -80,22 +79,23 @@ public class BPlusTreeNode implements HardwarePage {
             }
             return null;
         } else {
-            int index = binarySearch(this.keys, key);
-            int[] pointer = pointers.get(index);
-            BPlusTreeNode retrievedNode = buffer.read(template.getName(), pointer[0], true);
-            BPlusTreeNode newNode = retrievedNode.insert(key, pointer);
+            int index = binarySearch(keys, key);
+            int[] pointerFromIndex = pointers.get(index);
+            BPlusTreeNode retrievedNode = buffer.read(template.getName(), pointerFromIndex[0], true);
+            BPlusTreeNode newNode = retrievedNode.insert(key, pointer, buffer);
             if (newNode != null) {
-                this.keys.add(index, newNode.keys.get(0));
-                if (index + 1 = this.keys.size()) {
-                    //new node pointer
+                keys.add(index, newNode.keys.get(0));
+                if (index + 1 == keys.size()) {
+                    pointers.add(newNode.pointers.get(0));
+                } else {
+                    pointers.add(index + 1, newNode.pointers.get(0));
                 }
-                //new node pointer
                 if (keys.size() > maxSize - 1) {
-                    List<List<object>> splitKeys = splitArrayList(keys);
-                    List<List<int[]>> splitPointers = splitArrayList(pointers)
+                    List<List<Integer>> splitKeys = splitArrayList(keys);
+                    List<List<int[]>> splitPointers = splitArrayList(pointers);
                     this.keys = splitKeys.get(0);
                     this.pointers = splitPointers.get(0);
-                    BPlusTreeNode newNode = new BPlusTreeNode(this.isLeaf, this.maxSize);
+                    BPlusTreeNode newNode = new BPlusTreeNode(this.isLeaf, this.maxSize, this.template);
                     newNode.setKeys(splitKeys.get(1));
                     newNode.setPointers(splitPointers.get(1));
                     newNode.setParent(this.parent);
@@ -104,16 +104,15 @@ public class BPlusTreeNode implements HardwarePage {
             }
             return null;
         }
-
     }
 
-    public boolean delete(Object key, Buffer buffer, List<Object> keysDisplaced, List<Object> pointersDisplaced) throws Exception {
+    public boolean delete(int key, Buffer buffer, List<Object> keysDisplaced, List<Object> pointersDisplaced) throws Exception {
         if (isLeaf) {
             int index = keys.indexOf(key);
             if (index != -1) {
                 keys.remove(index);
                 pointers.remove(index);
-                if (keys.size() < maxSize/2) {
+                if (keys.size() < maxSize / 2) {
                     if (keysDisplaced != null && pointersDisplaced != null) {
                         collectChildren(keysDisplaced, pointersDisplaced, buffer);
                     }
@@ -124,25 +123,25 @@ public class BPlusTreeNode implements HardwarePage {
             }
             return false;
         } else {
-            int index = binarySearch(this.keys, key);
+            int index = binarySearch(keys, key);
             BPlusTreeNode nextNode = buffer.read(template.getName(), pointers.get(index)[0], true);
-            ArrayList<Object> keysD = new ArrayList();
-            ArrayList<int[]> pointersD = new ArrayList();
+            ArrayList<Object> keysD = new ArrayList<>();
+            ArrayList<int[]> pointersD = new ArrayList<>();
             if (nextNode.delete(key, buffer, keysD, pointersD)) {
-                // add free'd space to catalog
-                pointers.remove(index)
+                pointers.remove(index);
                 if (keys.size() == index) {
-                    keys.remove(index - 1)
+                    keys.remove(index - 1);
                 } else {
-                    keys.remove(index)
+                    keys.remove(index);
                 }
-                for (int i = 0; i < keysD.size(), i++) {
+                for (int i = 0; i < keysD.size(); i++) {
                     BPlusTreeNode newNode = this.insert();
                     if (newNode != null) {
-                        // write it to buffer
-                        // add to keys and pointers
+                        // Write it to buffer
+                        // Add to keys and pointers
+                    }
                 }
-                if (keys.size() < maxSize/2) {
+                if (keys.size() < maxSize / 2) {
                     if (keysDisplaced != null && pointersDisplaced != null) {
                         collectChildren(keysDisplaced, pointersDisplaced, buffer);
                     }
@@ -154,10 +153,9 @@ public class BPlusTreeNode implements HardwarePage {
     }
 
     public int search(int key) {
-        
         if (isLeaf) {
             int index = keys.indexOf(key);
-            return (index != -1) ? pointers.get(index) : -1;
+            return (index != -1) ? pointers.get(index)[0] : -1;
         } else {
             int index = 0;
             while (index < keys.size() && keys.get(index) <= key) {
@@ -165,61 +163,77 @@ public class BPlusTreeNode implements HardwarePage {
             }
             return children.get(index).search(key);
         }
-
     }
 
-    public byte[] toByte(int max_size){return null;}
-    public int bytesUsed(){return 0;}
-    public Table getTemplate(){return null;}
-    public int getPageNumber(){return 0;}
-    public void setPageNumber(int number){}
+    public void collectChildren(List<Object> keysCollected, List<Object> pointersCollected, Buffer buffer) {
+        if (isLeaf) {
+            for (int i = 0; i < keys.size(); i++) {
+                keysCollected.add(keys.get(i));
+                pointersCollected.add(pointers.get(i));
+            }
+        } else {
+            for (int i = 0; i < pointers.size(); i++) {
+                BPlusTreeNode nextNode = buffer.read(template.getName(), pointers.get(i)[0], true);
+                nextNode.collectChildren(keysCollected, pointersCollected, buffer);
+            }
+        }
+    }
 
-    public static <T extends Comparable<? super T>> int binarySearch(ArrayList<T> arrayList, T value) {
+    public static int binarySearch(List<Integer> arrayList, int value) {
         if (arrayList.isEmpty()) {
             return 0;
         }
-
         int left = 0;
         int right = arrayList.size() - 1;
-
         while (left <= right) {
             int mid = (left + right) / 2;
-            T midValue = arrayList.get(mid);
-
-            int comparison = value.compareTo(midValue);
-
-            if (comparison == 0) {
-                return mid + 1;
-            } else if (comparison < 0) {
-                right = mid - 1;
-            } else {
+            int midValue = arrayList.get(mid);
+            if (midValue == value) {
+                return mid;
+            } else if (midValue < value) {
                 left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
-
         return left;
     }
 
-    public static <T> List<List<T>> splitArrayList(ArrayList<T> arrayList) {
+    public static <T> List<List<T>> splitArrayList(List<T> arrayList) {
         int mid = arrayList.size() / 2;
-        
         List<List<T>> result = new ArrayList<>();
         result.add(arrayList.subList(0, mid));
         result.add(arrayList.subList(mid, arrayList.size()));
         return result;
     }
 
-    public void collectChildren(List<Object> keysCollected, List<Object> pointersCollected, Buffer buffer) {
-        if (isLeaf) {
-            for (int i = 0; i < keys.size(), i++) {
-                keysDisplaced.add(keys.get(i));
-                pointersDisplaced.add(pointers.get(i));
-            }
-        } else {
-            for (int i = 0; i < pointers.size(), i++) {
-                BPlusTreeNode nextNode = buffer.read(template.getName(), pointers.get(i)[0], true);
-                nextNode.collectChildren(keysCollected, pointersCollected, buffer);
-            }
-        }
+    @Override
+    public byte[] toByte(int max_size) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'toByte'");
+    }
+
+    @Override
+    public int bytesUsed() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'bytesUsed'");
+    }
+
+    @Override
+    public Table getTemplate() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getTemplate'");
+    }
+
+    @Override
+    public int getPageNumber() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPageNumber'");
+    }
+
+    @Override
+    public void setPageNumber(int number) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setPageNumber'");
     }
 }
