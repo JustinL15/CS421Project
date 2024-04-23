89c647ad45c7f88ad187abdef239258226a74660
Justin Lin <jl4682@rit.edu>
2024-04-22 23:38:33 -0400

    made compatible for all comparable types

--- java/structures/BPlusTreeNode.java
+++ java/structures/BPlusTreeNode.java
@@ -45,33 +45,33 @@ public class BPlusTreeNode<T extends Comparable<T>> implements HardwarePage {
         this.pointers = pointers;
     }
 
-    public BPlusTreeNode getParent() {
+    public BPlusTreeNode<T> getParent() {
         return parent;
     }
 
-    public void setParent(BPlusTreeNode parent) {
+    public void setParent(BPlusTreeNode<T> parent) {
         this.parent = parent;
     }
 
-    public List<BPlusTreeNode> getChildren() {
+    public List<BPlusTreeNode<T>> getChildren() {
         return children;
     }
 
-    public void setChildren(List<BPlusTreeNode> children) {
+    public void setChildren(List<BPlusTreeNode<T>> children) {
         this.children = children;
     }
 
-    public BPlusTreeNode insert(Object key, int[] pointer, Buffer buffer) {
+    public BPlusTreeNode<T> insert(T key, int[] pointer, Buffer buffer) {
         if (isLeaf) {
             int index = binarySearch(keys, key);
             keys.add(index, key);
             pointers.add(index, pointer);
             if (keys.size() > maxSize) {
-                List<List<Integer>> splitKeys = splitArrayList(keys);
+                List<List<T>> splitKeys = splitArrayList(keys);
                 List<List<int[]>> splitPointers = splitArrayList(pointers);
                 this.keys = splitKeys.get(0);
                 this.pointers = splitPointers.get(0);
-                BPlusTreeNode newNode = new BPlusTreeNode(this.isLeaf, this.maxSize, this.template);
+                BPlusTreeNode<T> newNode = new BPlusTreeNode<T>(this.isLeaf, this.maxSize, this.template);
                 newNode.setKeys(splitKeys.get(1));
                 newNode.setPointers(splitPointers.get(1));
                 newNode.setParent(this.parent);
@@ -81,8 +81,8 @@ public class BPlusTreeNode<T extends Comparable<T>> implements HardwarePage {
         } else {
             int index = binarySearch(keys, key);
             int[] pointerFromIndex = pointers.get(index);
-            BPlusTreeNode retrievedNode = (BPlusTreeNode) buffer.read(template.getName(), pointerFromIndex[0], true);
-            BPlusTreeNode newNode = retrievedNode.insert(key, pointer, buffer);
+            BPlusTreeNode<T> retrievedNode = buffer.read(template.getName(), pointerFromIndex[0], true);
+            BPlusTreeNode<T> newNode = retrievedNode.insert(key, pointer, buffer);
             if (newNode != null) {
                 keys.add(index, newNode.keys.get(0));
                 if (index + 1 == keys.size()) {
@@ -91,11 +91,11 @@ public class BPlusTreeNode<T extends Comparable<T>> implements HardwarePage {
                     pointers.add(index + 1, newNode.pointers.get(0));
                 }
                 if (keys.size() > maxSize - 1) {
-                    List<List<Integer>> splitKeys = splitArrayList(keys);
+                    List<List<T>> splitKeys = splitArrayList(keys);
                     List<List<int[]>> splitPointers = splitArrayList(pointers);
                     this.keys = splitKeys.get(0);
                     this.pointers = splitPointers.get(0);
-                    BPlusTreeNode newNode2 = new BPlusTreeNode(this.isLeaf, this.maxSize, this.template);
+                    BPlusTreeNode<T> newNode2 = new BPlusTreeNode<T>(this.isLeaf, this.maxSize, this.template);
                     newNode2.setKeys(splitKeys.get(1));
                     newNode2.setPointers(splitPointers.get(1));
                     newNode2.setParent(this.parent);
@@ -106,7 +106,7 @@ public class BPlusTreeNode<T extends Comparable<T>> implements HardwarePage {
         }
     }
 
-    public boolean delete(int key, Buffer buffer, List<Object> keysDisplaced, List<int[]> pointersDisplaced) throws Exception {
+    public boolean delete(T key, Buffer buffer, List<T> keysDisplaced, List<int[]> pointersDisplaced) throws Exception {
         if (isLeaf) {
             int index = keys.indexOf(key);
             if (index != -1) {
@@ -124,8 +124,8 @@ public class BPlusTreeNode<T extends Comparable<T>> implements HardwarePage {
             return false;
         } else {
             int index = binarySearch(keys, key);
-            BPlusTreeNode nextNode = buffer.read(template.getName(), pointers.get(index)[0], true);
-            ArrayList<Object> keysD = new ArrayList<>();
+            BPlusTreeNode<T> nextNode = buffer.read(template.getName(), pointers.get(index)[0], true);
+            ArrayList<T> keysD = new ArrayList<>();
             ArrayList<int[]> pointersD = new ArrayList<>();
             if (nextNode.delete(key, buffer, keysD, pointersD)) {
                 pointers.remove(index);
@@ -135,7 +135,7 @@ public class BPlusTreeNode<T extends Comparable<T>> implements HardwarePage {
                     keys.remove(index);
                 }
                 for (int i = 0; i < keysD.size(); i++) {
-                    BPlusTreeNode newNode = this.insert(keysD.get(i), pointersD.get(i), buffer);
+                    BPlusTreeNode<T> newNode = this.insert(keysD.get(i), pointersD.get(i), buffer);
                     if (newNode != null) {
                         // Write it to buffer
                         // Add to keys and pointers
@@ -165,7 +165,7 @@ public class BPlusTreeNode<T extends Comparable<T>> implements HardwarePage {
         }
     }
 
-    public void collectChildren(List<Object> keysCollected, List<int[]> pointersCollected, Buffer buffer) {
+    public void collectChildren(List<T> keysCollected, List<int[]> pointersCollected, Buffer buffer) {
         if (isLeaf) {
             for (int i = 0; i < keys.size(); i++) {
                 keysCollected.add(keys.get(i));
@@ -173,7 +173,7 @@ public class BPlusTreeNode<T extends Comparable<T>> implements HardwarePage {
             }
         } else {
             for (int i = 0; i < pointers.size(); i++) {
-                BPlusTreeNode nextNode = buffer.read(template.getName(), pointers.get(i)[0], true);
+                BPlusTreeNode<T> nextNode = buffer.read(template.getName(), pointers.get(i)[0], true);
                 nextNode.collectChildren(keysCollected, pointersCollected, buffer);
             }
         }
